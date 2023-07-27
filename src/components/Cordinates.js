8e1b23e46c66b083b7d998adf30c6dd122b49083
iron0120 <60773771+iron0120@users.noreply.github.com>
2023-07-27 15:14:29 +0700

    с обработчиком ошибки

--- src/components/Cordinates.js
+++ src/components/Cordinates.js
@@ -1,20 +1,40 @@
 import React, { useEffect, useState } from "react";
 import Cities from "./Cities";
+import City from "./City";
 
 function Cordinates() {
     const [lat, setLat] = useState([]);
     const [long, setLong] = useState([]);
+    const [posFalse, setPosFalse] = useState(false);
+    const [posTrue, setPosTrue] = useState(false);
+    const error = () => {
+        console.log('Невозможно получить ваше местоположение');
+        geo = navigator.geolocation
+        setPosFalse(true);
+        setPosTrue(false);
+    };
+
+    const success = (position) => {
+        setPosFalse(false);
+        setPosTrue(true);
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+        };
+
     useEffect(() => {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            setLat(position.coords.latitude);
-            setLong(position.coords.longitude);
-        });
+        if (!navigator.geolocation) {
+            console.log('Geolocation не поддерживается вашим браузером');
+        } else {
+            navigator.geolocation.getCurrentPosition(success, error,);
+        }
     }, [lat, long]);
+
 
 
     return (
         <div>
-            <Cities lat={lat} long={long} />
+            {posTrue &&<Cities lat={lat} long={long}/>}
+            {posFalse &&<City />}
 
         </div>
     )
